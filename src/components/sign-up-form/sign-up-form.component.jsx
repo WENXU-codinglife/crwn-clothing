3a6065a80d5746acf29a6ee2b81acd6bc663991a
wenxuw <xu_wen@sfu.ca>
2023-01-12 10:27:37 -0800

    button styling

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,7 @@ import { async } from '@firebase/util';
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
@@ -85,7 +86,7 @@ const SignUpForm = () => {
                     value={confirmPassword}
                 />
 
-                <button type='submit'>Sign Up</button>
+                <Button type='submit'>Sign Up</Button>
             </form>
         </div>
     )
