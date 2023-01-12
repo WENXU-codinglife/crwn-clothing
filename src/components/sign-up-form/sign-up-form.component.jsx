50c1fb80509147c4a3f670cbc4afd9df78f7079a
wenxuw <xu_wen@sfu.ca>
2023-01-12 10:05:50 -0800

    sign-up, userDoc creation and sign-in

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { async } from '@firebase/util';
 import { useState } from 'react';
-import { createAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
     displayName: '',
@@ -11,40 +13,77 @@ const defaultFormFields = {
 
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
-    const { displayName, email, password, confirmPassword} = formFields;
+    const { displayName, email, password, confirmPassword } = formFields;
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
 
     const handleSubmit = async (event) => {
-        event.preventDefault();
+        event.preventDefault(); // prevent any default actions from happening, that is, it only handles ...(look into it)
         if (password !== confirmPassword){
             alert("passwords do not match!");
             return;
         }
         try {
-            const response = await createAuthUserWithEmailAndPassword(email, password);
+            const { user } = await createAuthUserWithEmailAndPassword(email, password);
+            await createUserDocumentFromAuth(user, {displayName});
+            resetFormFields();
         } catch (e) {
-            console.log('ERROR: USER CREATION', e );
+            if (e.code === 'auth/email-already-in-use') {
+                alert('auth/email-already-in-use');
+            }else{
+                console.log('ERROR: USER CREATION', e );
+            }
         }
     }
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormFields({...formFields, [name]:value});
+        // console.log(event.target.value, displayName);
     }
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
-            <form onSubmit={() => {handleSubmit}}>
-                <label>Display Name</label>
-                <input type='text' required onChange={handleChange} name="displayName" value={displayName}></input>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
+        
+            <form onSubmit={handleSubmit}>
+                <FormInput 
+                    label="Display Name"
+                    type='text'
+                    required
+                    onChange={handleChange}
+                    name='displayName'
+                    value={displayName}
+                />
 
-                <label>Email</label>
-                <input type='email' required onChange={handleChange} name="email" value={email}></input>
+                <FormInput 
+                    label="Email"
+                    type='email'
+                    required
+                    onChange={handleChange}
+                    name='email'
+                    value={email}
+                />
 
-                <label>Password</label>
-                <input type='password' required onChange={handleChange} name="password" value={password}></input>
+                <FormInput 
+                    label="Password"
+                    type='password'
+                    required
+                    onChange={handleChange}
+                    name='password'
+                    value={password}
+                />  
 
-                <label>Confirm Password</label>
-                <input type='password' required onChange={handleChange} name="confirmPassword" value={confirmPassword}></input>
+                <FormInput 
+                    label="Confirm Password"
+                    type='password'
+                    required
+                    onChange={handleChange}
+                    name='confirmPassword'
+                    value={confirmPassword}
+                />
 
                 <button type='submit'>Sign Up</button>
             </form>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,7 +29,7 @@ export const signinwithGoogleRedirect = () => signinwithGoogleRedirect(auth, goo
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if (!userAuth) return;
     const userDocRef = doc(db, 'users', userAuth.uid); // if the db doesn't have a user collection, it will create it
     console.log(userDocRef);
@@ -43,7 +43,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation,
             });
         } catch (error) {
             console.log('error creating the user', error.message);
